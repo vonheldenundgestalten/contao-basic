69820e1d0774a942bfa07a7c3a36c6f4663ad24e
Markus Milkereit <markus.milkereit@magmell.de>
2020-09-26 18:42:53 +0200

    adding the video ratios to the settings

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,6 @@
+<?php
+
+// add new common video ratios
+$GLOBALS['TL_DCA']['tl_content']['fields']['playerAspect']['options'][] = '2:1';
+$GLOBALS['TL_DCA']['tl_content']['fields']['playerAspect']['options'][] = '1:1';
+
