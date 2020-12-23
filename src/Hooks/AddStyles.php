8eb7efe4f42b318805a6582c0a94b55097fa05ef
Marko Todić <drexplosiveweb@gmail.com>
2020-12-23 11:59:38 +0100

    Include styles on 'data protection' and 'imprint' pages

--- src/Hooks/AddStyles.php
+++ src/Hooks/AddStyles.php
@@ -2,7 +2,6 @@
 
 namespace Magmell\Contao\Basic\Hooks;
 
-use Contao\Config;
 use Contao\PageModel;
 use Contao\LayoutModel;
 use Contao\PageRegular;
@@ -10,25 +9,50 @@ use Contao\PageRegular;
 class AddStyles
 {
     /**
-     * @param string $strBuffer
-     * @return string $strTemplateName
-     * @return string
+     * @param PageModel $pageModel
+     * @param LayoutModel $layout
+     * @param PageRegular $pageRegular
      */
     public function generatePage(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular)
     {
-		// step 1: include CSS fixed (always)
-		// step 2: include only when needed, depending on page content & metadata
-		if(!is_array($GLOBALS['TL_CSS'])){
+		if (!is_array($GLOBALS['TL_CSS']))
+		{
 			$GLOBALS['TL_CSS'] = array();
 		}
+
 		array_unshift(
 			$GLOBALS['TL_CSS'],
 			"bundles/contaobasic/css/reset.scss|static",
-			"bundles/contaobasic/css/basic-imprint.scss|static",
 			"bundles/contaobasic/css/basic-video.scss|static",
 			"bundles/contaobasic/css/basic-maps.scss|static"
 		);
 
-        return;
+		$this->includeImprintCss();
+    }
+
+    /**
+     * Include styles on 'data protection' and 'imprint' pages
+     */
+    protected function includeImprintCss()
+    {
+        /** @var PageModel $objPage*/
+        global $objPage;
+
+        $arrAlias = explode('/', $objPage->alias);
+        $strAliasLastFragment = $arrAlias[count($arrAlias) - 1];
+
+        if (in_array($strAliasLastFragment, [
+            'datenschutz',
+            'impressum',
+            'legal-notice',
+            'data-protection',
+            'privacy'
+        ]))
+        {
+            array_unshift(
+                $GLOBALS['TL_CSS'],
+                "bundles/contaobasic/css/basic-imprint.scss|static"
+            );
+        }
     }
 }
